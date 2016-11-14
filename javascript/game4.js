bb6de11788ba0c0f9713fe2e8185982f885183a6
Faisal N <narsi.faisal@gmail.com>
2016-11-13 20:18:37 -0600

    game in progress

---
+++ javascript/game4.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+crystals = ['redCrystal.png','greenCrystal.png','purpleCrystal.jpg','whiteCrystal.png']	
+
+// Decalring Game Vars //
+
+var win = [];
+var losses =[];
+var redCrystal = [];
+var greenCrystal = [];
+var purpleCrystal = [];
+var whiteCrystal = [];
+
+
+// creating a function where crystal auto changes value //
+for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] == randomnumer) {
+
+    };
+}
+
+for (i = 0; i < numbers.length; i++) {
+			var imageCrystal = $('<img>');
+
